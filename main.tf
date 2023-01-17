cc3345bb6ce83601d9ca3f0359d5c1491f2e71e1
Swathi.Vijai <swathi.vijai@tigeranalytics.com>
2023-01-17 15:10:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  public_network_access_enabled = false
+
   account_kind             = "BlobStorage"
   
   tags = var.tags 
