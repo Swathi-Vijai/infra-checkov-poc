bfce78784f4b743a5af3da82d86be3470abf8b05
Swathi493 <swathi.vijai@tigeranalytics.local>
2023-01-17 12:15:34 +0530

    infra-checkov-poc

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.rsgrp
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+   
+  account_kind             = "StorageV2"
+  
+  tags = var.tags 
+}
+
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "rsgrp" {
+  type        = string
+  description = "Name of the resource group"
+  default     = "devops-rg"
+}
+
+variable "location" {
+  type        = string
+  description = "Location for deploying resources"
+  default     = "eastus"
+}
+variable "storage_account_name" {
+  type = string
+  description = "This variable defines storage account name"
+}
+
+
+variable "tags" {
+  type = object({
+    created_by       = list
+    created_for      = string
+    
+  })
+}
