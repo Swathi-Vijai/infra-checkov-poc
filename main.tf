738089ead51ceb5c8c6ece16934d97718565a1a8
sai.chodapaneedi <sai.chodapaneedi@tigeranalytics.com>
2023-01-19 10:14:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
-resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.rsgrp
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  account_kind             = "BlobStorage"
-  
-  tags = var.tags 
+resource "azurerm_resource_group" "appgrp" {
+  name     = local.resource_group_name
+  location = local.location  
 }
-
-
-
